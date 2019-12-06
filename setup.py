2af39d95d38356129236c9638a59d0535f604fa1
Steven Reagan <slreagan90@gmail.com>
2019-12-06 14:18:45 -0800

    corrected install_requires

--- setup.py
+++ setup.py
@@ -11,9 +11,8 @@ setup(
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+          'requests'
+  ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
