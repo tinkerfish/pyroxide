2f1f086fca2d38efc9b859a80bc2e5bad764be1b
Steven Reagan <slreagan90@gmail.com>
2019-12-06 14:23:48 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author_email = 'slreagan90@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/tinkerfish/pyroxide',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['PROXY', 'SOCKS', 'SANITIZE', 'VERIFY'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
   ],
@@ -19,8 +19,8 @@ setup(
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
